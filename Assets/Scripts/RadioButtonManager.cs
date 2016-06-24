3e987c420d8eba0e2f274ef3879765da746a31b2
Scott Bromander <scott@primeacademy.io>
2016-06-24 11:47:08 -0500

    initial commit

---
+++ Assets/Scripts/RadioButtonManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RadioButtonManager : MonoBehaviour {
+	private string currentDifficulty = "Easy";
+
+	public void PrintNewGroupValue(Toggle sender) {
+		if (sender.isOn) {
+			currentDifficulty = sender.tag;
+			print ("Option changed to: " + currentDifficulty);
+		}
+	}
+}
---
+++ Assets/Scripts/ToggleChangeManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ToggleChangeManager : MonoBehaviour {
+	private Toggle toggle;
+
+	// Use this for initialization
+	void Start () {
+		toggle = GetComponent<Toggle> ();
+	}
+
+	public void PrintNewToggleValue(){
+		bool status = toggle.isOn;
+		print ("Toggle is: " + status);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
